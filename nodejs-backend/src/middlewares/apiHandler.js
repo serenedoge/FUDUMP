ea1e08a8e69726faa4127a4503c49b137771c2de
serenedoge <andresavilas1993@gmail.com>
2020-11-29 17:25:43 -0500

    node code v2

--- nodejs-backend/src/middlewares/apiHandler.js
+++ nodejs-backend/src/middlewares/apiHandler.js
@@ -1,6 +1,7 @@
 fetch = require('node-fetch');
-const zeroXBaseUrl = `https://api.0x.org/swap/v1/quote?`
-const ethplorerBaseUrl = `https://api.ethplorer.io/getAddressInfo/`
+const zeroXBaseUrl = `https://api.0x.org/swap/v1/quote?`;
+const ethplorerBaseUrl = `https://api.ethplorer.io/getAddressInfo/`;
+const etherDecimals = '18';
 
 APIHandler = class {
 	constructor(cryptoAddress, apiKey) {
@@ -14,12 +15,32 @@ APIHandler = class {
                         let response = await fetch(baseUrl);
                         js = await response.json();
                         if (js.hasOwnProperty('code')) {
-                                throw new Error(JSON.stringify(js))
+                                throw new Error(js['reason'])
                         }
                 } catch(e) {
-                        console.log(e);
+                        //console.log(e);
+			return null;
                 }
                 return js;
+	}
+
+	async getAllZeroXQuotes(tokenAssets, buyToken) {
+		let keys = Object.keys(tokenAssets);
+		let transactions = [];
+		this.finTransactions = [];
+		for(let i = 0; i < keys.length; i++) {
+			transactions.push(this.getZeroXQuote(buyToken, keys[i], tokenAssets[keys[i]].balance));
+		}
+		this.finTransactions = await Promise.allSettled(transactions).then(function(results) {
+			let arr = {};
+			for (let i2 = 0; i2 < results.length; i2++) {
+				if (results[i2].value != null) {
+					arr[keys[i2]] = results[i2].value;
+				}
+			}
+			return arr;
+		});
+		console.log(this.finTransactions);
 	}
 
 	async getZeroXQuote(buyToken, sellToken, sellAmount) {
@@ -32,14 +53,17 @@ APIHandler = class {
 	async getEthplorer() {
 		let json = await this.getRestAPIJson(ethplorerBaseUrl + this.cryptoAddress + `?apiKey=` + this.apiKey);
 		let ethplorerJSON = {};
-		ethplorerJSON['ETH'] = json.ETH.balance;
+		ethplorerJSON['ETH'] = {'balance': json.ETH.balance * Math.pow(10, etherDecimals), 'decimals': etherDecimals};
 		for(let i = 0; i < json.tokens.length; i++) {
-			ethplorerJSON[json.tokens[i].tokenInfo.name] = json.tokens[i].balance/Math.pow(10, json.tokens[i].tokenInfo.decimals);
+			ethplorerJSON[json.tokens[i].tokenInfo.name] = {'balance': json.tokens[i].balance, 'decimals': json.tokens[i].tokenInfo.decimals};
 		}
-		console.log(ethplorerJSON);
 		return ethplorerJSON;
         }
 }
+async function Test() {
+	let api = new APIHandler(process.argv[2], process.argv[3]);
+	let walletAssets = await api.getEthplorer();
+	api.getAllZeroXQuotes(walletAssets, 'DAI');
+}
 
-api = new APIHandler(process.argv[2], process.argv[3]);
-api.getEthplorer();
+Test();
